50668848e5275d69c8d36b37ed3dd61f28bd177b
KevinTriPutra19 <kevintp@student.telkomuniversity.ac.id>
2026-07-26 02:51:22 +0700

    Test assets write

--- bank_sampah/test_write.php
+++ bank_sampah/test_write.php
@@ -1,9 +1,9 @@
 <?php
-$file = __DIR__ . "/assets/uploads/test.txt";
+$file = __DIR__ . "/assets/test.txt";
 if (@file_put_contents($file, "test") !== false) {
-    echo "Write OK\n";
+    echo "Write assets/ OK\n";
     unlink($file);
 } else {
-    echo "Write FAILED\n";
+    echo "Write assets/ FAILED\n";
 }
 
